77110377162ba2b22206998728f84c34a88b6430
Agnaldo Marinho <agnaldoneto@ufpa.br>
2017-11-07 02:31:59 -0300

    Adcionando Hello world usando cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/agnaldoneto/estudos/rust-estudo/hello_cargo/target/debug/hello_cargo: /home/agnaldoneto/estudos/rust-estudo/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/agnaldoneto/estudos/rust-estudo/hello_cargo/target/release/hello_cargo: /home/agnaldoneto/estudos/rust-estudo/hello_cargo/src/main.rs
